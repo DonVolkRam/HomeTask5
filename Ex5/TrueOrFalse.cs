61cc787d8d0f012a3957052e1887679fb2750875
DonVolkRam <success.vka@gmail.com>
2018-10-05 17:55:01 +0300

    Начало выполнения 5 задания

---
+++ Ex5/TrueOrFalse.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex5
+{
+
+    class TrueOrFalse
+    {
+        string[] Question;
+        string[] Answer;
+        int NumOfQuiz;
+        List <int> RandomIndexes = new List<int>();
+
+        public string[] Question1 { get => Question; set => Question = value; }
+        public string[] Answer1 { get => Answer; set => Answer = value; }
+        public List<int> RandomIndexes1 { get => RandomIndexes; set => RandomIndexes = value; }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Инициализация базы вопросов");
+            try
+            {
+                TrueOrFalse Quiz = new TrueOrFalse();
+                if (Quiz.Read() == false)
+                    throw new Exception("Error load file");
+                for (int i = 0; i < 5; i++)
+                {
+                    int Index = Quiz.Chose();
+                    Console.WriteLine(Quiz.Question[Index]);
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Ошибка инициализации");
+            }
+            finally
+            {
+                Console.Read();
+            }
+        }
+
+        /// <summary>
+        /// Выбор случайного вопроса без повторений
+        /// </summary>
+        /// <returns>Возвращает номер вопроса</returns>
+        public int Chose()
+        {
+            Random rnd = new Random();
+            bool Compare = true;
+            int RndIndex = 0;           
+            
+            while (Compare)
+            {
+                RndIndex = rnd.Next(0, 38);
+                for (int i = 0; i < RandomIndexes.LongCount(); i++)
+                {
+                    if (RndIndex == RandomIndexes.ElementAt(i))
+                        Compare = true;
+                }
+                Compare = false;
+            }
+            RandomIndexes.Add(RndIndex);
+            return RndIndex;
+        }
+        /// <summary>
+        /// Чтение из файлов вопросов
+        /// </summary>
+        /// <returns>Возвращает истину если успешное чтение</returns>
+        public bool Read()
+        {
+            string filename = "..\\..\\Ex5_Qwiz.txt";
+            string[] allFile;
+            string[] temp;
+            if (File.Exists(filename))
+            {
+                //Считываем все строки в файл
+                allFile = File.ReadAllLines(filename);
+                NumOfQuiz = allFile.Length;
+                Question1 = new string[NumOfQuiz];
+                Answer1 = new string[NumOfQuiz];
+                for (int i = 0; i < NumOfQuiz; i++)
+                {
+                    temp = allFile[i].Split(';');
+                    if (temp.Length > 3)
+                    {
+                        Console.WriteLine("Ошибка чтения данных");
+                        continue;
+                    }
+                    Question1[i] = temp[1];
+                    Answer1[i] = temp[2];
+                }
+                return true;
+            }
+            else
+            {
+                Question1 = null;
+                Answer1 = null;
+                Console.WriteLine("Файл не найден");
+                return false;
+            }
+
+        }
+    }
+
+    
+}
